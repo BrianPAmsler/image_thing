a3dcfbcaf365597c2fc4b19af5c0adebbc75debf
Brian Amsler <manof2words@yahoo.com>
2022-09-07 00:03:39 -0700

    merging

--- src/main.rs
+++ src/main.rs
@@ -15,12 +15,9 @@ fn print_bytes(bytes: &[u8]) {
 static TEST_STRING: &str = "Hello World!";
 
 fn main() {
-    let mut img = Image::create_image_from_file("C:\\Users\\Brian\\Desktop\\blank2.png");
-    let pixel = img.get_pixel_value(0, 0);
-    println!("pixel: ({:#04X}, {:#04X}, {:#04X}, {:#04X})", pixel.0, pixel.1, pixel.2, pixel.3);
+    let mut img = Image::create_image_from_file("C:\\Users\\Brian\\Desktop\\test.png");
 
     let mut coder = ImageCoder::create(&mut img);
-    coder.num_bits(4);
     let mut data = vec![0u8; TEST_STRING.len()];
 
     coder.read(&mut data[..]);
